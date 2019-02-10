bfb9a5f5157f7257d190439291512cdfa58221a8
Fomin Artem <terra.integer@gmail.com>
2019-02-10 23:07:10 +0200

    Add project files.

---
+++ ICMarkets.CodingChallenge.Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICMarkets.CodingChallenge.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
